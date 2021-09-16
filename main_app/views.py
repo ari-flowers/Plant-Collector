0fa68a736359874cb6a487e313346dd1284da96b
ARI-FLOWERS <ARI.WEEDON@GMAIL.COM>
2021-09-15 20:17:54 -0700

    Got the basic server up and running - purring like a kitten

---
+++ main_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ🪴</h1><br/><p>Welcome to the Plant Collector</p>')
