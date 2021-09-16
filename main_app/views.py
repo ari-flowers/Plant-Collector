983d770f82ee45c26dda0ff3fb728f96f901d8f3
ARI-FLOWERS <ARI.WEEDON@GMAIL.COM>
2021-09-15 23:49:06 -0700

    Added database functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
+from .models import Plant
 
 # Add the plant class & list and view function below the imports
-class Plant:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, species, description, age):
-    self.name = name
-    self.species = species
-    self.description = description
-    self.age = age
-
-plants = [
-  Plant('Epipremnum Pinatum Variegata', 'Pothos', 'Grown from a node.', 0),
-  Plant('Pink Princess', 'Philodendron', '3 inch PPP', 1),
-  Plant('Pink Splash', 'Syngonium', 'Top 5 faves', 1)
-]
+# class Plant:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, species, description, age):
+#     self.name = name
+#     self.species = species
+#     self.description = description
+#     self.age = age
+#
+# plants = [
+#   Plant('Epipremnum Pinatum Variegata', 'Pothos', 'Grown from a node.', 0),
+#   Plant('Pink Princess', 'Philodendron', '3 inch PPP', 1),
+#   Plant('Pink Splash', 'Syngonium', 'Top 5 faves', 1)
+# ]
+def plants_index(request):
+  plants = Plant.objects.all()
+  return render(request, 'plants/index.html', { 'plants': plants })
 
 # Define the home view
 def home(request):
@@ -21,7 +25,3 @@ def home(request):
 # Define about view
 def about(request):
     return render(request, 'about.html')
-
-# Define index view
-def plants_index(request):
-    return render(request, 'plants/index.html', { 'plants': plants })
