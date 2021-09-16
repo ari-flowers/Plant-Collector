74f5bfd38abfd00e42a799463205b82498eec2be
ARI-FLOWERS <ARI.WEEDON@GMAIL.COM>
2021-09-15 21:06:58 -0700

    got list index functionality working

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
-# Add the following import
-from django.http import HttpResponse
+# Add the plant class & list and view function below the imports
+class Plant:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+plants = [
+  Plant('Lolo', 'tabby', 'foul little demon', 3),
+  Plant('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Plant('Raven', 'black tripod', '3 legged plant', 4)
+]
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ🪴</h1><br/><p>Welcome to the Plant Collector</p>')
+  return render(request, 'home.html')
+
+# Define about view
+def about(request):
+    return render(request, 'about.html')
+
+# Define index view
+def plants_index(request):
+    return render(request, 'plants/index.html', { 'plants': plants })
